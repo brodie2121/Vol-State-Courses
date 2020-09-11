434182f3fa6d6526575bd294a3d30e73fd534fc8
Brodie Brock <brodiebrock@gmail.com>
2020-09-11 18:24:59 -0500

    finishing assignment 3

--- Intro to programming and Logic/brock_brodie_assign3.py
+++ Intro to programming and Logic/brock_brodie_assign3.py
@@ -1,9 +1,12 @@
 
+# how much do you make an hour?
 hourlyRate = float(input("What is your hourly rate: "))
+# how many hours did you work this week?
 hoursWorked = float(input("How many hours did you work: "))
-# if hoursWorked > 40:
-#pay = (hourlyRate)
 
-pay = (hourlyRate * hoursWorked)
+if hoursWorked <= 40:
+    pay = hoursWorked * hourlyRate
+elif hoursWorked > 40:
+    pay = ((hoursWorked-40)*hourlyRate*1.5)+hourlyRate*40
 
 print("The total weekly number is ", format(pay, '.2f'))
