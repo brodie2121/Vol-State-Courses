f3033407f71e700da9b9a98d9e1b91196894ee95
Brodie Brock <brodiebrock@gmail.com>
2020-09-15 19:42:07 -0500

    starting assignment 4

---
+++ Intro to programming and Logic/brock_brodie_assign4.py
@@ -0,0 +1,13 @@
+num = 3-15
+if (num % 2) == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+number = int(input("Enter the monster number: "))
+
+if (number % 2) == 0:
+    print("The monster color is black")
+else:
+    print("The monster color is blue")
