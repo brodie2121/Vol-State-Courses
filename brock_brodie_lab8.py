60ccfaafb048168c83ba98beea9eb84162ffcef2
Brodie Brock <brodie.brock@eyeconic.tv>
2020-10-22 20:32:49 -0500

    lab 8

---
+++ brock_brodie_lab8.py
@@ -0,0 +1,12 @@
+# testing to see if two integers are equal
+def is_equal(x, y):
+    if x == y:
+        return("equal")
+    else:
+        return("not equal")
+
+
+# printing answer for two integers
+print(is_equal(5, 10))
+print(is_equal(33, 33))
+print(is_equal(21, 12))
