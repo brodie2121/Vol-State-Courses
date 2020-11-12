1684c2a45ed5c612aff515a1845e9968283088be
Brodie Brock <brodie.brock@eyeconic.tv>
2020-11-11 19:50:07 -0600

    "chapter 11 lab"

---
+++ Intro to programming and Logic/brock_brodie_lab11.py
@@ -0,0 +1,19 @@
+# Creating my_list
+my_list = ["baseball", "soccer", "hockey",
+           "golf", "football", "basketball", "criket"]
+
+# Adding a new item to my_list
+for i in range(1):
+    my_list.append("volleyball")
+
+# Iterating over my_list, printing each item
+print(my_list)
+
+# Printing the length of my_list
+print(len(my_list))
+
+# Deleting an item off of my_list
+del my_list[1:2]
+
+# Printing new length of my_list
+print(len(my_list))
