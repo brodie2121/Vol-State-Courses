ee09bc6622ffb11d1fb2a09dd65b58e66baa84e4
Brodie Brock <brodiebrock@gmail.com>
2020-12-01 19:31:00 -0600

    updating lab 14

--- Intro to programming and Logic/brock_brodie_lab14.py
+++ Intro to programming and Logic/brock_brodie_lab14.py
@@ -7,10 +7,10 @@ regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
 
 # validating Email
-def check(email):
+def check(is_valid_email):
 
     # passing the regular expression
-    if(re.search(regex, email)):
+    if(re.search(regex, is_valid_email)):
         print("true")
 
     else:
@@ -21,9 +21,9 @@ def check(email):
 if __name__ == '__main__':
 
     # Enter the email
-    email = "brodiebrock@volstate.edu"
+    is_valid_email = "brodiebrock@volstate.edu"
     # calling run function
-    check(email)
+    check(is_valid_email)
 
-    email = "johndoe!volstate.edu"
-    check(email)
+    is_valid_email = "johndoe!volstate.edu"
+    check(is_valid_email)
