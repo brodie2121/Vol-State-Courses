cbada2279148de858e813a8fadbf5417025c60f0
Brodie Brock <brodiebrock@gmail.com>
2021-01-22 16:20:56 -0600

    Starting C# Class"

---
+++ C#/PersonalInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+class Lyrics
+{
+    static void Main()
+    {
+        WriteLine("It's empty in the valley of your heart");
+        WriteLine("The sun, it rises slowly as you walk");
+        WriteLine("Away from all the fears");
+        WriteLine("And all the faults you've left behind");
+    }
+}
